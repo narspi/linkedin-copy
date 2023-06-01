3395c045b043ac28aa3ddc392c84f08b86e5ac9d
narspi <ivanovmichurina@gmail.com>
2023-06-01 11:40:59 +0300

    start

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZrFNT7aXh11EmdG4DnnBL8jpuJHf9j6U",
+  authDomain: "corporate-messenger-299bf.firebaseapp.com",
+  projectId: "corporate-messenger-299bf",
+  storageBucket: "corporate-messenger-299bf.appspot.com",
+  messagingSenderId: "1039516266168",
+  appId: "1:1039516266168:web:65b67144b09156fe6e0e4d"
+};
+
+const app = initializeApp(firebaseConfig);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <div className="bg-gray-500">
+      test tailwind
+    </div>
+  );
+};
+
+export default Home;
